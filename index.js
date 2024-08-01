772e87e6a798501ba499597981ed1847222e1e69
phumebyte <phumelele.dlamini1000@gmail.com>
2024-08-01 21:29:35 +0200

    Wrote number generator function and card storage array

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let hasBlackjack = false
 let isAlive = true
+// array for storing cards
+let cards = [
+    firstCard,
+    secondCard
+]
 
 let sum = firstCard + secondCard
 
@@ -9,13 +14,30 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// function generates a random number
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()* 11) + 1
+    if(randomNumber === 1){
+        return 11
+    } else if(randomNumber > 9){
+        return 10
+    } else{
+        return randomNumber
+    }
+}
+
+// this function is called by the start game click on the html
 function startGame(){
     renderGame()
 }
 
 function renderGame(){
-cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+cardsEl.textContent = "Cards :"       // JS removes the default text written on the html
 sumEl.textContent = "Sum: " + sum
+
+for (let i=0; i< cards.length; i++){
+    cardsEl.textContent += cards[i]
+}
 
     if (sum <21 ){
         message = "Do you want to draw a new card?"
@@ -34,7 +56,8 @@ sumEl.textContent = "Sum: " + sum
 
 function newCard(){
     messageEl.textContent = "Drawing a new card from the deck"
-    let card = 7
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
     renderGame()
 }
